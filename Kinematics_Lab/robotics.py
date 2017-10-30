73c3588b43b0990cbe0646f419e9859a4ee4bf11
Francisco Martins <franciscoprelhaz.m@gmail.com>
2017-10-30 21:28:37 +0000

    set

--- Kinematics_Lab/robotics.py
+++ Kinematics_Lab/robotics.py
@@ -4,6 +4,8 @@ from numpy.linalg import inv
 from math import sqrt, pi, cos, sin, pow, atan2, asin, acos
 
 # a, alpha, d, theta
+#mixed variables missing
+
 DOF0 = [0, -pi/2, 99]
 DOF1 = [120, 0, 0]
 DOF2_aux = [0, 0, 0]
